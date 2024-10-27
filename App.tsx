5251aa656b21af34f92757b6c5990dd604917d5c
Luis Carlos [FRONT] <luisestrela.bol951@gmail.com>
2024-10-27 14:32:21 -0300

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
+import { SignIn } from '@/screens/sign-in';
+import { theme } from '@/styles/theme';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <SignIn />
+      <StatusBar />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/sign-in/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import * as Styles from './styles';
+
+export function SignIn() {
+  return (
+    <Styles.Container>
+      <Text>SignIn</Text>
+    </Styles.Container>
+  );
+}
